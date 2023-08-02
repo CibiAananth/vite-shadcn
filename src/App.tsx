bf465b1a9739b496cd63152f48fb05b182af4d9e
Cibi Aananth <cibiaananth@gmail.com>
2023-08-02 11:14:03 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button';
+
+function App() {
+  return (
+    <>
+      <div className="p-5">
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
